a592b9eb2c7b763b3c2cc44215ff572263d560a1
Graciela <gtorres6316@gmail.com>
2025-09-20 19:01:02 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleMDB;
+public class Program
+{
+    public static async Task Main()
+    {
+        App app = new App();
+        await app.Start();
+    }
+}
---
+++ src/App.cs
@@ -0,0 +1,44 @@
+using System.Net;
+using System.Text;
+namespace SimpleMDB;
+public class App
+{
+    private HttpListener server;
+    public App()
+    {
+        string host = "http://127.0.0.1:8080/";
+        server = new HttpListener();
+        server.Prefixes.Add(host);
+        Console.WriteLine("Server listening on... " + host);
+    }
+    public async Task Start()
+    {
+        server.Start();
+        while (server.IsListening)
+        {
+            var ctx = server.GetContext();
+            await HandleContextAsync(ctx);
+        }
+    }
+    public void Stop()
+    {
+        server.Stop();
+        server.Close();
+    }
+    private async Task HandleContextAsync(HttpListenerContext ctx)
+    {
+        var req = ctx.Request;
+        var res = ctx.Response;
+        if (req.HttpMethod == "GET" && req.Url!.AbsolutePath == "/")
+        {
+            string html = "Hello!";
+            byte[] content = Encoding.UTF8.GetBytes(html);
+            res.StatusCode = (int)HttpStatusCode.OK;
+            res.ContentEncoding = Encoding.UTF8;
+            res.ContentType = "text/plain";
+            res.ContentLength64 = content.LongLength;
+            await res.OutputStream.WriteAsync(content);
+            res.Close();
+        }
+    }
+}
