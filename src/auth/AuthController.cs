e1a60cc6f540405befda4526111eda3fb400b992
Graciela <gtorres6316@gmail.com>
2025-09-23 16:02:11 -0400

    Templates added

--- src/auth/AuthController.cs
+++ src/auth/AuthController.cs
@@ -13,13 +13,6 @@ public class AuthController
 
     public async Task LandingPageGet(HttpListenerRequest req, HttpListenerResponse res, Hashtable options)
     {
-string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!");
-            byte[] content = Encoding.UTF8.GetBytes(html);
-            res.StatusCode = (int)HttpStatusCode.OK;
-            res.ContentEncoding = Encoding.UTF8;
-            res.ContentType = "text/html";
-            res.ContentLength64 = content.LongLength;
-            await res.OutputStream.WriteAsync(content);
-            res.Close();
-    }
+        string html = HtmlTemplates.Base("SimpleMDB", "Landing Page", "Hello World!2");
+        await HttpUtils.Respond(req, res, options, (int) HttpStatusCode.OK, html);    }
 }
